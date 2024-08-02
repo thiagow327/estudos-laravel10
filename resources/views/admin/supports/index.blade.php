292d5bf24997305ce0796d0abd6654b862d82306
thiago.silva <thiagosilva327@live.com>
2024-08-02 11:40:09 -0300

    feat: use layout blade in index page to forum

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,6 @@
-<h1>Listagem de duvidas</h1>
+@extends('admin/supports/layout')
+@section('index')
+    <h2>Duvidas</h2>
+    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its
+        layout.</p>
+@stop
---
+++ resources/views/admin/supports/layout.blade.php
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Forum</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Forum de Duvidas</h1>
+    </header>
+    <section class="menu">
+        <ul>
+            <li><a href="#">>Pagina Inicial</a></li>
+            <li><a href="#">>Profissionais</a></li>
+            <li><a href="#">>Ajuda</a></li>
+        </ul>
+    </section>
+    <section class="content">
+        @yield('index')
+    </section>
+    <script src="jquery-3.0.0.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
