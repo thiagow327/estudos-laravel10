6443a68a8ad4b826a6ff90f19188e30dd18d6318
thiago.silva <thiagosilva327@live.com>
2024-08-02 10:46:51 -0300

    first-commit and new route to contact

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Site\SiteController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// get view in folder
+// Route::get('/contact', function(){
+//     return view('site/contact');
+// });
+
+// get view in controller
+Route::get('/contact', [SiteController::class, 'contact']);
