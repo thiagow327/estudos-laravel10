44666856232e33a3beec9f61ec0b87c38e04be55
thiago.silva <thiagosilva327@live.com>
2024-08-02 11:16:43 -0300

    feat: add route, controller and view to support for forum

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,6 @@ Route::get('/', function () {
 
 // get view in controller
 Route::get('/contact', [SiteController::class, 'contact']);
+
+// use name for reference
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
