5bb8788761c7bd6f470d3f89bb57a8efd73474cb
thiago.silva <thiagosilva327@live.com>
2024-08-02 11:44:15 -0300

    fix: detele code not use

--- resources/views/admin/supports/layout.blade.php
+++ resources/views/admin/supports/layout.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Forum</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -20,8 +19,6 @@
     <section class="content">
         @yield('index')
     </section>
-    <script src="jquery-3.0.0.min.js"></script>
-    <script src="script.js"></script>
 </body>
 
 </html>
