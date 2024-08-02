86caa3e76dd52c09bb8c5fbe8b07d1980ee3571c
thiago.silva <thiagosilva327@live.com>
2024-08-02 16:34:31 -0300

    feat: add migration, seeder and models. / feat: controller get data db and send for view

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support) // dependence injection, create object automatic
     {
-        return view('admin/supports/index');
+        $supports = $support->all();
+        dd($supports); //Dump and Die
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
